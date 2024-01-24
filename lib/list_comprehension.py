bd287fabf2829b2b747376026f315ee349a10d8e
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-01-24 02:23:12 -0500

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [i for i in range(0,num_list,2)]
 
 def make_exclamation(sentence_list):
-    pass
+    return [i +"!" for i in sentence_list]
+
+
+print(return_evens(10))
+
+print(make_exclamation(["bird", "i am cat"]))
